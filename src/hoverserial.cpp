3f2f14109dfdc0ea2d0e3bffe6c39bbdea469826
Jan L <janlauw@gmx.de>
2023-12-05 20:37:10 +0100

    change rxtx

--- src/hoverserial.cpp
+++ src/hoverserial.cpp
@@ -26,8 +26,8 @@
 
 // ########################## DEFINES ##########################
 #define HOVER_SERIAL_BAUD 115200 // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
-#define RXPIN 23                 // GPIO 4 => RX for Serial1
-#define TXPIN 22                 // GPIO 5 => TX for Serial1
+#define RXPIN 22                 // GPIO 4 => RX for Serial1
+#define TXPIN 23                 // GPIO 5 => TX for Serial1
 #define START_FRAME 0xABCD       // [-] Start frme definition for reliable serial communication
 #define TIME_SEND 100            // [ms] Sending time interval
 #define SPEED_MAX_TEST 300       // [-] Maximum speed for testing
